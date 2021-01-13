1488e9ac37c2cb558683cb6a765d681e63abca5d
aguljahan <71317430+aguljahan@users.noreply.github.com>
2021-01-13 11:00:35 +0300

    start from index.php

--- project/index.php
+++ project/index.php
@@ -2,12 +2,6 @@
 	session_start();
 	include "config.php";
 
-	  $sql_statement = "SELECT o.userId FROM orders o
-											WHERE o.userId = (SELECT MAX(userId) AS uIdm FROM customers)";
-		$result = mysqli_query($db, $sql_statement);
-		$row = mysqli_fetch_assoc($result);
-		if(!is_null($row['userId'])){
-
 			$sql_statement = "INSERT INTO customers(isGuest) VALUES(0)";
 			$r = mysqli_query($db, $sql_statement);
 
@@ -16,25 +10,5 @@
 
 			$roww = mysqli_fetch_assoc($rr);
 			$_SESSION["customerId"] = $roww['uIdm'];
-		}else{
-			$sql_statement = "SELECT MAX(userId) AS uIdm FROM customers";
-			$rr = mysqli_query($db, $sql_statement);
-			$row = mysqli_fetch_assoc($rr);
-
-			$delCustomer = $row['uIdm'];
-			$sql_statement = "DELETE FROM customers
-												WHERE userId = $delCustomer";
-			$result = mysqli_query($db, $sql_statement);
-
-			$sql_statement = "INSERT INTO customers(isGuest) VALUES(0)";
-			$r = mysqli_query($db, $sql_statement);
-
-			$sql_statement = "SELECT MAX(userId) AS uIdm FROM customers";
-			$rr = mysqli_query($db, $sql_statement);
-
-			$roww = mysqli_fetch_assoc($rr);
-			$_SESSION["customerId"] = $roww['uIdm'];
-
-		}
- header("Location: homepage.php");
+      header("Location: homepage.php");
 ?>
